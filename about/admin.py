722f1b58d4ab65f579f5243c663b64d0d6c5ed91
2ndborn <micah.francis69@googlemail.com>
2024-04-26 21:01:50 +0000

    created the Models and admin files

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import About_me, Education, Work_experience, Interest
 
-# Register your models here.
+class About_meAdmin(admin.ModelAdmin):
+    list_display = (
+        'content',
+        'image',
+    )
+
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = (
+        'from_date',
+        'to_date',
+        'institution',
+        'qualifation',
+    )
+
+
+class Work_experienceAdmin(admin.ModelAdmin):
+    list_display = (
+        'from_date',
+        'to_date',
+        'job_title',
+        'company',
+        'location',
+        'achievement1',
+        'achievement2',
+        'achievement3',
+        'achievement4',
+        'achievement5',
+    )
+
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = (
+        'interest1',
+        'interest2',
+        'interest3',
+        'interest4',
+        'interest5',
+    )
+
+
+admin.site.register(About_me, About_meAdmin)
+admin.site.register(Education, EducationAdmin)
+admin.site.register(Work_experience, Work_experienceAdmin)
+admin.site.register(Interest, InterestAdmin)
--- about/models.py
+++ about/models.py
@@ -1,5 +1,49 @@
 from django.db import models
 
-class about_me(models.Model):
+class About_me(models.Model):
+
+    class Meta:
+        verbose_name = 'About'
+
     content = models.TextField(max_length=500, blank=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Education(models.Model):
+    from_date = models.DateTimeField(blank=False)
+    to_date = models.DateTimeField(blank=False)
+    institution = models.CharField(max_length=100, blank=False)
+    qualifation = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.qualifation
+
+
+class Work_experience(models.Model):
+    from_date = models.DateTimeField(blank=False)
+    to_date = models.DateTimeField(blank=True)
+    job_title = models.CharField(max_length=25, blank=False)
+    company = models.CharField(max_length=25, blank=False)
+    location = models.CharField(max_length=25, blank=False)
+    achievement1 = models.TextField(max_length=25, blank=False)
+    achievement2 = models.TextField(max_length=25, blank=False)
+    achievement3 = models.TextField(max_length=25, blank=False)
+    achievement4 = models.TextField(max_length=25, blank=False)
+    achievement5 = models.TextField(max_length=25, blank=False)
+
+    def __str__(self):
+        return self.job_title
+
+class Interest(models.Model):
+    interest1 = models.TextField(max_length=100, blank=False)
+    interest2 = models.TextField(max_length=100, blank=False)
+    interest3 = models.TextField(max_length=100, blank=False)
+    interest4 = models.TextField(max_length=100, blank=True)
+    interest5 = models.TextField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.interest1
