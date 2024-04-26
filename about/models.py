7637ce404099fd6d94252579d29b7d9ecc90874b
2ndborn <micah.francis69@googlemail.com>
2024-04-26 19:03:16 +0000

    started the about me Model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class about_me(models.Model):
+    content = models.TextField(max_length=500, blank=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
