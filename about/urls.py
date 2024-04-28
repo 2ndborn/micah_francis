e16eb3db191234b4c905b9ce729928192efee323
2ndborn <micah.francis69@googlemail.com>
2024-04-28 17:36:36 +0000

    Added edit functionality to the education section

--- about/urls.py
+++ about/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.about, name='about'),
     path('add_education/', views.add_education, name='add_education'),
+    path('edit_education/<int:education_id>/', views.edit_education, name='edit_education'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Education, Work_experience, Interest
 
 from .forms import EducationForm
 
 def about(request):
-    """A view to render the about me page"""
+   """A view to render the about me page"""
 
-    education = Education.objects.all()
-    work_experience = Work_experience.objects.all()
-    interests = Interest.objects.all()
+   education = Education.objects.all()
+   work_experience = Work_experience.objects.all()
+   interests = Interest.objects.all()
 
-    context = {
-       'education': education,
-       'interests': interests,
-       'work_experience': work_experience,
-    }
+   context = {
+      'education': education,
+      'interests': interests,
+      'work_experience': work_experience,
+   }
 
-    return render(request, 'about/about.html', context)
+   return render(request, 'about/about.html', context)
 
 
 def add_education(request):
@@ -37,3 +37,23 @@ def add_education(request):
    }
 
    return render(request, template, context)
+
+
+def edit_education(request, education_id):
+   """A view to edit Education entries"""
+
+   education = get_object_or_404(Education, pk=education_id)
+   if request.method == 'POST':
+      form = EducationForm(request.POST, request.FILES, instance=education)
+      if form.is_valid():
+         form.save()
+         return redirect(reverse('about'))
+   
+   else:
+      form = EducationForm(instance=education)
+
+   context = {
+      'form': form,
+   }
+
+   return render(request, 'about/edit_education.html', context)
