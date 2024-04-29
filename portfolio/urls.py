69deb11503a6e63b4f23a37f8058d8ec642df676
2ndborn <micah.francis69@googlemail.com>
2024-04-29 19:43:21 +0000

    added crud functionality to portfolio app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
     path('add_portfolio/', views.add_portfolio, name='add_portfolio'),
+    path('edit_portfolio/<int:portfolio_id>/', views.edit_portfolio, name='edit_portfolio'),
+    path('delete_portfolio/<int:portfolio_id>/', views.delete_portfolio, name='delete_portfolio'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -33,3 +33,32 @@ def add_portfolio(request):
     }
 
     return render(request, template, context)
+
+
+def edit_portfolio(request, portfolio_id):
+    """A view to render the edit Portfolio page"""
+
+    portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+    if request.method == 'POST':
+        form = PortfolioForm(request.POST, request.FILES, instance=portfolio)
+        if form.is_valid():
+            form.save()
+        return redirect(reverse('portfolio'))
+   
+    else:
+        form = PortfolioForm(instance=portfolio)
+
+    template = 'portfolio/edit_portfolio.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def delete_portfolio(request, portfolio_id):
+   """A view to delete Education entries"""
+
+   portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+   portfolio.delete()
+   return redirect(reverse('about'))
