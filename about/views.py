db956d3cc82835e0c16d8ec41990eab1729890ba
2ndborn <micah.francis69@googlemail.com>
2024-05-06 15:06:41 +0000

    styling the toast

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import About, Education, Work_experience, Interest
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import AboutForm, EducationForm, Work_experienceForm, InterestForm
 
@@ -26,12 +27,14 @@ def about(request):
 def edit_about(request, about_id):
    """A view to edit the About section"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    about = get_object_or_404(About, pk=about_id)
    if request.method == 'POST':
       form = AboutForm(request.POST, request.FILES, instance=about)
       if form.is_valid():
          form.save()
+         messages.info(request, 'About me updated!')
          return redirect(reverse('about'))
    
    else:
@@ -49,11 +52,13 @@ def edit_about(request, about_id):
 def add_education(request):
    """A view to add to the Education section"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    if request.method == 'POST':
       form = EducationForm(request.POST, request.FILES)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Education added!')
          return redirect(reverse('about'))
    
    else:
@@ -71,12 +76,14 @@ def add_education(request):
 def edit_education(request, education_id):
    """A view to edit Education entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    education = get_object_or_404(Education, pk=education_id)
    if request.method == 'POST':
       form = EducationForm(request.POST, request.FILES, instance=education)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Education updated!')
          return redirect(reverse('about'))
    
    else:
@@ -94,9 +101,11 @@ def edit_education(request, education_id):
 def delete_education(request, education_id):
    """A view to delete Education entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    education = get_object_or_404(Education, pk=education_id)
    education.delete()
+   messages.info(request, 'Education deleted!')
    return redirect(reverse('about'))
 
 
@@ -104,11 +113,13 @@ def delete_education(request, education_id):
 def add_work(request):
    """A view to add Work Experience entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    if request.method == 'POST':
       form = Work_experienceForm(request.POST, request.FILES)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Work Experience added!')
          return redirect(reverse('about'))
    else:
       form = Work_experienceForm()
@@ -125,12 +136,14 @@ def add_work(request):
 def edit_work(request, work_id):
    """A view to edit Work experience entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    work = get_object_or_404(Work_experience, pk=work_id)
    if request.method == 'POST':
       form = Work_experienceForm(request.POST, request.FILES, instance=work)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Work Experience updated!')
          return redirect(reverse('about'))
    
    else:
@@ -148,9 +161,11 @@ def edit_work(request, work_id):
 def delete_work(request, work_id):
    """A view to delete a Work Experience entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    work = get_object_or_404(Work_experience, pk=work_id)
    work.delete()
+   messages.info(request, 'Work Experience deleted!')
    return redirect(reverse('about'))
 
 
@@ -158,11 +173,13 @@ def delete_work(request, work_id):
 def add_interest(request):
    """A view to add to the Interest section"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    if request.method == 'POST':
       form = InterestForm(request.POST, request.FILES)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Interest added!')
          return redirect(reverse('about'))
    
    else:
@@ -180,12 +197,14 @@ def add_interest(request):
 def edit_interest(request, interest_id):
    """A view to edit Interest entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    interest = get_object_or_404(Interest, pk=interest_id)
    if request.method == 'POST':
       form = InterestForm(request.POST, request.FILES, instance=interest)
       if form.is_valid():
          form.save()
+         messages.info(request, 'Interest updated!')
          return redirect(reverse('about'))
    
    else:
@@ -202,7 +221,9 @@ def edit_interest(request, interest_id):
 def delete_interest(request, interest_id):
    """A view to delete Interest entries"""
    if not request.user.is_superuser:
-        return redirect(reverse('home'))
+      messages.error(request, 'Sorry, you are not authorised to view this page')
+      return redirect(reverse('home'))
    interest = get_object_or_404(Interest, pk=interest_id)
    interest.delete()
+   messages.info(request, 'Interest deleted!')
    return redirect(reverse('about'))
