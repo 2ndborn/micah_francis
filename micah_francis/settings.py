98d4ef544a51f2241f1ade5234bc9968385d6247
2ndborn <micah.francis69@googlemail.com>
2024-05-07 12:57:36 +0000

    removed secret key

--- micah_francis/settings.py
+++ micah_francis/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2o_=tnh7_vkse72y6m0a^1+6+!vwpz@0o_(hl$x@p%gq0qbk9m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-2ndborn-micahfrancis-hhjghrrn4qg.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['micah-francis-d557d3c34851.herokuapp.com', '8000-2ndborn-micahfrancis-hhjghrrn4qg.ws-eu111.gitpod.io']
 
 
 # Application definition
