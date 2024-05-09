f0c22fa4575b1d6d82d3e93b1ab99d54aa36df8a
2ndborn <micah.francis69@googlemail.com>
2024-05-09 17:26:40 +0000

    amending the contact views

--- contact/views.py
+++ contact/views.py
@@ -7,31 +7,30 @@ from django.core.mail import send_mail
 
 def contact(request):
    """A view to render the contact page"""
-   contact = ContactForm(request.POST)
-   if request.method == 'POST':
-      if contact.is_valid:
+   contact = ContactForm(request.POST or None)
+   if request.method == 'POST' and contact.is_valid():
          name = request.POST['name']
          subject = request.POST['subject']
          message = request.POST['message']
          email = request.POST['email']
          recipient_list = [settings.EMAIL_HOST_USER]
 
-      send_mail(
-         subject,
-         message,
-         email,
-         recipient_list,
-         fail_silently=False,
-      )
-      messages.success(request, 'Your message has been sent!')
+         send_mail(
+            subject,
+            message,
+            email,
+            recipient_list,
+            fail_silently=False,
+         )
+         messages.success(request, 'Your message has been sent!')
 
-      template = 'contact/contact.html'
-      context = {
-         'name': name,
-         'contact': contact,
-      }
-      return render(request, template, context)
-   
-   else:
-      messages.error(request, 'There was an error.')
-      return render(request, 'contact/contact.html', {'contact': contact})
+         template = 'contact/contact.html'
+         context = {
+            'name': name,
+            'contact': contact,
+         }
+         return render(request, template, context)
+      else:
+         if request.method == 'POST':
+            messages.error(request, 'There was an error with your form.')
+         return render(request, 'contact/contact.html', {'contact': ContactForm()})
