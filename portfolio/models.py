ef28a6f7354a8b27f0eaa23b60093e4301be725a
2ndborn <micah.francis69@googlemail.com>
2024-05-12 18:53:31 +0000

    amended portfolio models from charfield to urlfield

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,6 +6,6 @@ class Portfolio(models.Model):
     title = models.CharField(max_length=25, blank=False)
     description = models.TextField(max_length=500, blank=False)
     technologies = models.TextField(max_length=250, blank=False)
-    web_address = models.CharField(max_length=500, blank=False)
+    web_address = models.URLField(max_length=500, blank=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
