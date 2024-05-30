a7af219b5da87a3cb29bc8c88145b791d4f641f8
2ndborn <micah.francis69@googlemail.com>
2024-05-30 16:48:48 +0000

    Finished adding sound to the contact form

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,18 @@ function PlaySound(soundId) {
     let form = document.getElementById('conForm');
 
     form.addEventListener("submit", function (event) {
-        if(form.checkValidity()) {
+        if(form.reportValidity()) {
+            event.preventDefault();  // Prevent the form from submitting
             thissound.play();
+            thissound.volume = 0.2;  // Play the sound
+            thissound.playbackRate = 0.5;  // Play the sound
+
+            // Submit the form after the sound has finished playing
+            thissound.onended = function() {
+                form.submit();
+            }
         }
     });
-  }
+}
 
-  PlaySound('whoosh');
+PlaySound('whoosh');
